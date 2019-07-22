a6ecde327f9c821f524e13c9763a75cecd64bc8b
Luis Lucana <luislucana@gmail.com>
2019-07-21 22:41:54 -0300

    Commit inicial.

---
+++ src/main/java/br/com/bluesoft/desafio/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package br.com.bluesoft.desafio.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.bluesoft.desafio.model.Produto;
+
+public interface ProdutoRepository extends CrudRepository<Produto, String> {
+
+	public Produto findByGtin(String gtin);
+}
